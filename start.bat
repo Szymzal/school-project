ec3d301a05a135446f2f2f212804567ee6fecfa4
uczen1 <uczen1@tech.local>
2022-11-14 16:11:59 +0100

    Small change in script

--- start.bat
+++ start.bat
@@ -1,4 +1,6 @@
 @echo off
 git config --global --unset user.name
 git config --global --unset user.email
+git config --unset user.name
+git config --unset user.email
 rundll32.exe keymgr.dll,KRShowKeyMgr
